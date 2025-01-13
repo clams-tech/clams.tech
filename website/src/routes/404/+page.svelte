a8e77a43002e4e16204a6422857053f5a3825fd6
Aaron <abarnard@protonmail.com>
2025-01-14 07:20:35 +1100

    Add 404 page

---
+++ website/src/routes/404/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import ContentContainer from '$lib/components/shared/ContentContainer.svelte';
+</script>
+
+<ContentContainer title="Page Not Found">
+	<div class="flex items-center justify-center p-6">
+		<a href="/">Go Home</a>
+	</div>
+</ContentContainer>
