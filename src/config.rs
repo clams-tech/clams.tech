344e4efa025bba147ac5d5ad1bfd02307a749292
Aaron <abarnard@protonmail.com>
2025-02-17 19:33:01 -0600

    Add discord webhook payment notifications

--- src/config.rs
+++ src/config.rs
@@ -22,6 +22,11 @@ pub struct Config {
     #[arg(long, env = "PHOENIXD_PASSWORD")]
     /// Password for Phoenixd server
     pub phoenixd_password: String,
+    #[arg(long, env = "PHOENIXD_WEBHOOK_SECRET")]
+    /// Webhook secret for Phoenixd server to verify webhook payloads
+    pub phoenixd_webhook_secret: String,
     #[arg(long, env = "DOMAIN")]
     pub domain: String,
+    #[arg(long, env = "DISCORD_WEBHOOK_URL")]
+    pub discord_webhook_url: String,
 }
